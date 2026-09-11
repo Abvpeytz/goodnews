4172d0281c72dd7e1cbb9ccbf44e928893bff445
Andreas B. Vestergaard <abv@peytz.dk>
2026-09-11 10:37:12 +0200

    fix(theme): added editable bloks

--- src/components/storyblok/Article.jsx
+++ src/components/storyblok/Article.jsx
@@ -1,6 +1,8 @@
+import { storyblokEditable } from "@storyblok/react/rsc";
+
 export default function Article({ blok }) {
   return (
-    <div className="article">
+    <div className='article' {...storyblokEditable(blok)}>
       <span>{blok.name}</span>
     </div>
   );
--- src/components/storyblok/Hero.jsx
+++ src/components/storyblok/Hero.jsx
@@ -1,6 +1,11 @@
+import { storyblokEditable } from "@storyblok/react/rsc";
+
 export default function Hero({ blok }) {
   return (
-    <section className='relative flex flex-col justify-center bg-foreground text-white p-5 mb-6 rounded-lg shadow-lg min-h-80 overflow-hidden'>
+    <section
+      className='relative flex flex-col justify-center bg-foreground text-white p-5 mb-6 rounded-lg shadow-lg min-h-80 overflow-hidden'
+      {...storyblokEditable(blok)}
+    >
       <div className='flex flex-col gap-2'>
         <h1 className='font-mono text-5xl font-black'>{blok.title}</h1>
         <p className='max-w-md'>{blok.description}</p>
